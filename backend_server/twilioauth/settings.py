82ad58c807006d4885d40a6ef0646f4c3c5bf516
John-Kimani <kimwaweru100@gmail.com>
2023-05-29 18:58:38 +0300

    feat: configure new application

--- backend_server/twilioauth/settings.py
+++ backend_server/twilioauth/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    ##Third-party apps
+    'authentication.apps.AuthenticationConfig',
+    
 ]
 
 MIDDLEWARE = [
