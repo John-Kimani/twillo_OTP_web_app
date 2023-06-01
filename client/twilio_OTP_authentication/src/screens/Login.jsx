22025b41931c572c8e01c5a3280c30c595a00598
John-Kimani <kimwaweru100@gmail.com>
2023-06-01 13:44:12 +0300

    feat: revamp login UI

--- client/twilio_OTP_authentication/src/screens/Login.jsx
+++ client/twilio_OTP_authentication/src/screens/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
 import NavBar from '../components/NavBar';
 import LoginEmail from '../components/LoginEmail';
 import LoginPhone from '../components/LoginPhone';
@@ -13,30 +14,34 @@ function Login() {
         setPhoneLogin(current => !current);
     };
 
-    if (phoneLogin) {
-        return (
-            <>
-                <NavBar />
-                <LoginPhone />
-                <div className="text-center mt-2">
-                    <hr/>
-                    <Button variant='secondary' onClick={togglePhoneLogin}>Login with Email</Button>
-                    <p>or</p>
-                    <p>Don&apos;t have an account? <Link to='/sign-up'>Register</Link></p>
-                </div>
-            </>)
+    const toggleEmailLogin = () => {
+        setPhoneLogin(false)
     }
 
     return (
         <>
             <NavBar />
-            <LoginEmail />
-            <div className="text-center mt-2">
-                <hr />
-                <Button variant='secondary' onClick={togglePhoneLogin}>Login with Phone</Button>
-                <p>or</p>
-                <p>Don&apos;t have an account? <Link to='/sign-up'>Register</Link></p>
+            <div className="row">
+                <div className="col-md-4"></div>
+                <div className="col-md-4 mt-4">
+                    <Card>
+                    <Card.Header>
+                        <div className="d-flex justify-content-around">
+                        <Button variant='outline-secondary' onClick={toggleEmailLogin}>Login with Email</Button>
+                        <Button variant='outline-secondary' onClick={togglePhoneLogin}>Login with Phone</Button>
+                        </div>
+                    </Card.Header>
+                    <Card.Body>
+                        {phoneLogin ? <LoginPhone/> : <LoginEmail />}
+                    </Card.Body>
+                    <Card.Footer className='text-center'>
+                        <p>Don&apos;t have an account? <Link to='/sign-up'>Register</Link></p>
+                    </Card.Footer>
+                    </Card>
+                </div>
+                <div className="col-md-4"></div>
             </div>
+
         </>
     )
 }
