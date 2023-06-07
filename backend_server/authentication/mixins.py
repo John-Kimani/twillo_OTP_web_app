88c7aa774b3f0c403b95ed5d79c5521aa9851cd8
John-Kimani <kimwaweru100@gmail.com>
2023-06-07 16:05:34 +0300

    feat: update MessageHandler class

--- backend_server/authentication/mixins.py
+++ backend_server/authentication/mixins.py
@@ -4,29 +4,36 @@ import random
 
 class MessageHandler:
     phone_number = None
+    token = None
+    client_email_address = None
 
-    def __init__(self, phone_number) -> None:
-        self.phone_number = phone_number
+    def __init__(self, phone_number, client_email_address, token) -> None:
+        pass
+        # self.phone_number = phone_number
+        # self.client_email_address = client_email_address
+        # self.token = token
 
-    def send_otp_via_message(self):
+    def send_otp_via_message(phone_number):
         client = Client(settings.ACCOUNT_SID, settings.AUTH_TOKEN)
         service = client.verify.services(settings.VERIFY_SERVICE)
-        service.verifications.create(
-            to=f'{settings.COUNTRY_CODE}{self.phone_number}',
+        result = service.verifications.create(
+            to=f'{settings.COUNTRY_CODE}{phone_number}',
             channel='sms'
         )
+        print(f'Phone: {result.status}')
 
-    def send_otp_via_email(self, email):
+    def send_otp_via_email(client_email_address):
         client = Client(settings.ACCOUNT_SID, settings.AUTH_TOKEN)
         service = client.verify.services(settings.VERIFY_SERVICE)
-        service.verifications.create(
-            to=email,
+        result = service.verifications.create(
+            to=client_email_address,
             channel='email'
         )
+        print(f'Phone: {result.status}')
 
-    def verify_token(self, phone_number, token):
+    def verify_SMS_token(phone_number, token):
         client = Client(settings.ACCOUNT_SID, settings.AUTH_TOKEN)
         service = client.verify.services(settings.VERIFY_SERVICE)
-        result = service.verification_checks.create(to=phone_number, code=token)
+        result = service.verification_checks.create(to=f'{settings.COUNTRY_CODE}{phone_number}', code=token)
 
         print(result.status)
